e3541489cf8a81302ea819e77d67dc9d1d724b4f
James Dumbell <james.dumbell@infinityworks.com>
2020-05-20 16:18:06 +0100

    .

--- main.tf
+++ main.tf
@@ -93,3 +93,23 @@ resource "google_storage_bucket" "bucket-prices" {
     enabled     = true
   }
 }
+
+resource "google_compute_router" "crypto_router" {
+  name    = "crypto-router"
+  region  = var.region
+  network = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_router_nat" "crypto_route_nat" {
+  name                               = "crypto-route-nat"
+  router                             = google_compute_router.crypto_router.name
+  region                             = google_compute_router.crypto_router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
