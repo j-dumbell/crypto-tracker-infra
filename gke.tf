b862cecadc34912d01e6fc30d5d3f4fce7deb526
James Dumbell <james.dumbell@infinityworks.com>
2020-05-21 14:21:26 +0100

    add autoscaling

---
+++ gke.tf
@@ -0,0 +1,38 @@
+resource "google_container_cluster" "gke_cluster" {
+  name                      = "crypto-tracker-cluster"
+  location                  = var.zone
+  remove_default_node_pool  = true
+  initial_node_count        = 1
+  network                   = google_compute_network.vpc_network.self_link
+  subnetwork                = google_compute_subnetwork.general_subnet.self_link
+
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false
+    master_ipv4_cidr_block  = "10.2.0.0/28"
+  }
+
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block = ""
+    services_ipv4_cidr_block = ""
+  }
+
+}
+
+resource "google_container_node_pool" "gke_node_pool" {
+  name       = "gke-node-pool"
+  cluster    = google_container_cluster.gke_cluster.name
+
+  autoscaling {
+    max_node_count = 2
+    min_node_count = 0
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_write"
+    ]
+  }
+}
--- main.tf
+++ main.tf
@@ -59,41 +59,6 @@ resource "google_sql_user" "backend_db_user" {
   password = data.google_secret_manager_secret_version.pgpassword.secret_data
 }
 
-resource "google_container_cluster" "gke_cluster" {
-  name                      = "crypto-tracker-cluster"
-  location                  = var.zone
-  remove_default_node_pool  = true
-  initial_node_count        = 1
-  network                   = google_compute_network.vpc_network.self_link
-  subnetwork                = google_compute_subnetwork.general_subnet.self_link
-
-  private_cluster_config {
-    enable_private_nodes    = true
-    enable_private_endpoint = false
-    master_ipv4_cidr_block  = "10.2.0.0/28"
-  }
-
-  ip_allocation_policy {
-    cluster_ipv4_cidr_block = ""
-    services_ipv4_cidr_block = ""
-  }
-
-}
-
-resource "google_container_node_pool" "gke_node_pool" {
-  name       = "gke-node-pool"
-  cluster    = google_container_cluster.gke_cluster.name
-  node_count = 2
-
-  node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/devstorage.read_write"
-    ]
-  }
-}
-
 resource "google_storage_bucket" "bucket-prices" {
   name          = "crypto-tracker-prices"
   location      = "EU"
